4b205e7bcb19af0f2e93ea1619452d883fbfab6c
mmindbreak <mmindbreak@proton.me>
2022-12-29 16:45:36 +0100

    not really sure why I did what I did, but the solution was just removing code that just did not belong.

--- Leetcode/ReverseLinkedList.cs
+++ Leetcode/ReverseLinkedList.cs
@@ -2,15 +2,18 @@ using System.Security.Cryptography;
 
 namespace Leetcode;
 
-fpublic class ListNode {
+public class ListNode
+{
     public int val;
     public ListNode? next;
-     
-    public ListNode(int val=0, ListNode next=null) {
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
         this.val = val;
         this.next = next;
     }
 }
+
 public class ReverseLinkedList
 {
     public ListNode ReverseList(ListNode head)
@@ -24,22 +27,14 @@ public class ReverseLinkedList
         ListNode cur = head;
         ListNode? prev = null;
 
-        while (cur.next != null)
+        while (cur != null)
         {
-            if (prev == null)
-            {
-                prev = cur;
-                cur = cur.next;
-                continue;
-            }
             var next = cur.next;
             cur.next = prev;
             prev = cur;
             cur = next;
         }
 
-        cur.next = prev;
-        
-        return cur;
+        return prev;
     }
 }
